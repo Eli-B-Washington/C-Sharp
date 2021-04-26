fec6c5063702d7b9882defd1e4641eb5fc503431
eli-b-washington <bea14018@byui.edu>
2021-04-26 14:32:27 -0700

    commit step 2 changes

--- MathQuiz/MathQuiz/Form1.cs
+++ MathQuiz/MathQuiz/Form1.cs
@@ -10,18 +10,24 @@ using System.Windows.Forms;
 
 namespace MathQuiz
 {
-    public partial class plusLeftLabel : Form
+    public partial class Form1 : Form
     {
-        public plusLeftLabel()
+        Random randomizer = new Random();
+        int addend1;
+        int addend2;
+        public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
 
         private void timeLable_Click(object sender, EventArgs e)
         {
 
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -30,6 +36,50 @@ namespace MathQuiz
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void plusLeftLabel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plusLeftLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void StartTheQuiz()
+        {
+            // Fill in the addition problem.
+            // Generate two random numbers to add.
+            // Store the values in the variables 'addend1' and 'addend2'.
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            // Convert the two randomly generated numbers
+            // into strings so that they can be displayed
+            // in the label controls.
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+            Console.WriteLine(plusLeftLabel.Text);
+
+            // 'sum' is the name of the NumericUpDown control.
+            // This step makes sure its value is zero before
+            // adding any values to it.
+            sum.Value = 0;
+        }
+
+   
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
         }
     }
 }
